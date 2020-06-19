1740ad8a5a2622e3feb6f2af927d638705c0dd1c
Aous <aous.mohammad97@gmal.com>
2020-06-19 14:06:05 -0400

    add

--- application/views/admin/start.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $this->session->userdata('user_name');
-$email = $this->session->userdata('user_email');
-$pass = $this->session->userdata('user_password');
-$id = $this->session->userdata('user_id');
-include_once "template/header.php";
-include_once "template/navbar.php";
-include_once "pages/" . $pageName.".php";
-include_once "template/footer.php";
--- application/views/start.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $data = $this->Home_model->get_all();
-	include_once "template/header.php";
-    include_once $pageName.".php";
-    include_once "template/footer.php";
-?>
